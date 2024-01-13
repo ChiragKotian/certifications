ae5971b3924e4e373fcfe6701af2c6d0b338bf4a
Chirag Hareesha Kotian <chirag.kotian2007@gmail.com>
2024-01-13 21:42:38 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,32 +4,7 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-/*progress bar start*/
-setTimeout(() => {
-	let progressBar = document.getElementById("myProgressBar");
-  
-	let barwidth = 10;
-  
-	let ab = setInterval(progressFunction, 40);
-  
-	function progressFunction() {
-	  if (barwidth >= 100) {
-		clearInterval(ab);
-		setTimeout(() => {
-		  progressBar.classList.add("height-0");
-		}, 3000);
-		setTimeout(() => {
-		  document.getElementById("loader").classList.add("hide-loader");
-		}, 4000);
-	  } else {
-		barwidth++;
-		progressBar.style.width = barwidth + "%";
-		document.getElementById("incrementpercentage").innerHTML = barwidth + "%";
-	  }
-	}
-  }, 1000);
 
-  /*progress bar end*/
 
 (function($) {
 
